c8ed635a47efaab80a063e24c6fd410ca8cc4d5d
c9summit <100874920+c9summit@users.noreply.github.com>
2022-06-08 09:05:45 +1200

    2nd Window

--- Harsahil Basra - CompSci Internal.py
+++ Harsahil Basra - CompSci Internal.py
@@ -1,3 +1,31 @@
 #Harsahil Basra
 #07/06/22
 #Julie's Party Hire Code
+#This program is so Julie can keep a track of which items are being hired
+
+#import tkinter to make a GUI
+from tkinter import*
+
+
+#Create 1st window for entry
+root = Tk()
+root.title("Julie's hire store")
+
+def open():
+    top = Toplevel()
+    top.title('Customer Information')
+    btn2 = Button(top, text="close window", command=top.destroy).pack()
+
+#Create button to take user to 2nd window
+btn = Button(root, text="Open Second Window", command=open).pack()
+
+root.geometry('400x200')
+
+#Add title of store and other aesthetics such as colour and font
+
+
+
+
+
+
+mainloop()

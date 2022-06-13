168aa0dcba77934073a572ce765849fc7ee80d81
c9summit <100874920+c9summit@users.noreply.github.com>
2022-06-13 19:08:16 +1200

    Close button now closes both windows

--- Harsahil Basra - CompSci Internal.py
+++ Harsahil Basra - CompSci Internal.py
@@ -4,14 +4,17 @@
 #This program is so Julie can keep a track of which items are being hired
 
 #import tkinter to make a GUI
+from tabnanny import check
 from tkinter import*
 from tkinter import ttk
 
+#these are the global variables 
+global hireinfo, entry_customername, entry_receiptnumber, entry_itemhired, entry_noofitems, firstwindow, secondwindow
 
 #Create 1st window for entry
-root = Tk()
-root.title("Julie's Hire Store")
-root.geometry('400x200')
+firstwindow = Tk()
+firstwindow.title("Julie's Hire Store")
+firstwindow.geometry('400x200')
 
 
 
@@ -19,31 +22,38 @@ root.geometry('400x200')
 def windowtwo():
     secondwindow = Toplevel()
     secondwindow.title('Customer Information')
-    secondwindow.geometry('400x200')
+    #Create empty labels, buttons, entryboxes and putting them in correct grid location
     #Customer's full name text
-    Label(secondwindow,text="Customer Full Name").grid(column=0, row=1)
+    Label(secondwindow,text="Customer Full Name").grid(column=0, row=2)
     #Create textbox for customer's fullname where the user can input 
     entry_customername = Entry(secondwindow)
-    entry_customername.grid(column=1, row=1)
+    entry_customername.grid(column=1, row=2)
     #Receipt number's text
-    Label(secondwindow, text="Receipt Number").grid(column=0,row=2)
+    Label(secondwindow, text="Receipt Number").grid(column=0,row=3)
     entry_receiptnumber = Entry(secondwindow)
-    entry_receiptnumber.grid(column=1, row=2)
+    entry_receiptnumber.grid(column=1, row=3)
     #Item hired text
-    Label(secondwindow, text="Item Hired").grid(column=0, row=3)
+    Label(secondwindow, text="Item Hired").grid(column=0, row=4)
     entry_itemhired = Entry(secondwindow)
-    entry_itemhired.grid(column=1, row=3)
+    entry_itemhired.grid(column=1, row=4)
     #Number Of Items Hired text
-    Label(secondwindow, text="No. Of Items Hired").grid(column=0, row=4)
+    Label(secondwindow, text="No. Of Items Hired").grid(column=0, row=5)
     entry_noofitems = Entry(secondwindow)
-    entry_noofitems.grid(column=1, row=4)
+    entry_noofitems.grid(column=1, row=5)
     #Add quit button
-    Button(secondwindow, text="Quit", command=secondwindow.destroy).grid(column=3,row=0, sticky=N+E, padx=45)
+    def quit():
+        firstwindow+secondwindow.destroy()
+    Button(secondwindow, text="Close", command=secondwindow.quit).grid(column=3,row=0, sticky=N+E, padx=45)
+    #Adding append button
+    Button(secondwindow, text="Append Info",
+           command=check) .grid(column=1, row=6)
+
 
 
 
 #Create button to take user to 2nd window
-Button(root, text="Open Second Window", command=windowtwo).pack()
+Button(firstwindow, text="Open Second Window", command=windowtwo).pack()
+
 
 
 
